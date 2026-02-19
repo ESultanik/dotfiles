789bc5b677ef1d055472425895617cf79a67d4d9
Evan Sultanik <evan@sultanik.com>
2026-02-19 10:58:59 -0500

    Switch to `vim` as the default git editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    
+    set -gx GIT_EDITOR vim
 
     function dotfiles
         git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
