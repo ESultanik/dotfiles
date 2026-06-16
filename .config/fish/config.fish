6e0019a9cbfe9b97eed8a6ced95c239258af0370
Evan Sultanik <evan@sultanik.com>
2026-06-16 11:03:55 -0400

    Support for fnm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,10 @@ end
 
 fish_add_path -g --append --path /usr/local/bin
 fish_add_path -g --append --path ~/.local/bin
+
+alias codex-cyber-preview='codex -m gpt-5.5-cyber-preview --config model_reasoning_effort="xhigh"'
+alias codex-cyber='codex -m gpt-5.4-cyber --config model_reasoning_effort="xhigh"'
+
+if type -q fnm
+    fnm env --use-on-cd | source
+end
